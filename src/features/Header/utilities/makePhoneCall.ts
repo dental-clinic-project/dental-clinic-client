df8cbee8e575ccd3dafb8cc2bb9753c1de030da4
Denis Matveev <matveevdenis458@gmail.com>
2024-10-22 09:22:48 +0300

    deleted files

--- src/features/Header/utilities/makePhoneCall.ts
+++
@@ -1,5 +0,0 @@
-export const makePhoneCall = (phoneNumber: string) => {
-  const telLink = `tel:${phoneNumber}`;
-  
-  window.location.href = telLink;
-};
--- src/widgets/index.ts
+++
@@ -1,5 +0,0 @@
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
-import Intro from './Intro/Intro';
-
-export { Footer, Header, Intro };
